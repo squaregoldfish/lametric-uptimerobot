ba59d7f8f2b746fd8e6728ff1604a2f02741b403
Steve Jones <steve@squaregoldfish.co.uk>
2022-12-12 20:59:47 +0100

    Improve error handling

--- lametric-uptimerobot-monitors.py
+++ lametric-uptimerobot-monitors.py
@@ -3,8 +3,63 @@
 import sys,cgi,cgitb
 import traceback
 import json
-import utr
+import requests
 cgitb.enable()
+
+def _getmonitors_(apikey):
+  url = 'https://api.uptimerobot.com/v2/getMonitors'
+
+  payload = 'api_key=' + apikey + '&format=json&logs=0'
+  headers = {
+    'content-type': 'application/x-www-form-urlencoded',
+    'cache-control': 'no-cache'
+  }
+
+  response = requests.request('POST', url, data=payload, headers=headers)
+  if response.status_code != 200:
+    raise Exception("HTTP ERROR " + str(response.status_code))
+
+  data = json.loads(response.text)
+  stat = data['stat']
+  if stat == 'fail':
+    if data['error']['type'] == 'invalid_parameter' and data['error']['parameter_name'] == 'api_key':
+      raise Exception('Invalid API key')
+    else:
+      raise Exception(data['error']['type'])
+
+  return data['monitors']
+
+def getmonitorcounts(apikey, ignore_paused):
+  error = None
+  up = 0
+  down = 0
+  paused = 0
+  unknown = 0
+  total = 0
+
+  try:
+    monitors = _getmonitors_(apikey)
+
+    for monitor in monitors:
+      status = monitor['status']
+      if status == 0:
+        if not ignore_paused:
+          paused = paused + 1
+          total = total + 1
+      else:
+        total = total + 1
+        if status == 2:
+          up = up + 1
+        elif status == 8 or status == 9:
+          down = down + 1
+        else:
+          unknown = unknown + 1
+
+  except Exception as e:
+    print(traceback.format_exc())
+    error = e
+
+  return [error, up, down, paused, unknown, total]
 
 error = None
 up = 0
@@ -21,7 +76,7 @@ try:
   if apikey is None:
     error = 'Missing apikey'
   else:
-    monitor_info = utr.getmonitorcounts(apikey, ignore_paused)
+    monitor_info = getmonitorcounts(apikey, ignore_paused)
 
     error = monitor_info[0]
     up = monitor_info[1]
@@ -38,8 +93,13 @@ except Exception as e:
 sys.stdout.write('Content-type: application/json\n\n')
 
 if error is not None:
+  with open('/home/squaregoldfish/temp/lametric-uptimerobot-monitors.err', 'a') as f:
+    f.write(arguments)
+    f.write('\n')
+    f.write(repr(error))
+    f.write('\n')
+
   sys.stdout.write('{"frames":[{"text":"Cannot retrieve Uptime Robot status","icon":"i23080"}]}')
-  sys.stderr.write(traceback.format_exc())
 else:
   output = {}
   output['frames'] = []
